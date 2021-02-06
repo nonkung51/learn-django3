d82737a144a931dd1c0d343336e382bad98014f7
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-02-06 11:31:28 +0700

    change model display to title

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
