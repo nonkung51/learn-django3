6a4c51b4234d5490ff86de0e958fad0cb00565ec
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-02-06 16:42:25 +0700

    add post detail views

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ def hello(request):
     all_posts = Post.objects.all()
 
     return render(request, 'blog/home.html', { 'all_posts': all_posts })
+
+def post_detail(request, id):
+    single_post = Post.objects.get(id=id)
+
+    return render(request, 'blog/post-detail.html', { 'single_post': single_post })
